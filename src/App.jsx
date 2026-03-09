5aa3b9959170b07fd4c7a91ef49a7e1d1af8979a
Dartey Natasha <darteynatasha409@gmail.com>
2026-03-09 22:52:46 +0000

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import Home from "./pages/Home";
+import Explore from "./pages/Explore";
+import AssetDetail from "./pages/AssetDetail";
+import Learn from "./pages/Learn";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
-  return <Outlet />;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="explore" element={<Explore />} />
+        <Route path="assets/:id" element={<AssetDetail />} />
+        <Route path="learn" element={<Learn />} />
+      </Route>
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
 }
 
 export default App;
